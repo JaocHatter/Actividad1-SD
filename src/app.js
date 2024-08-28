48d14b3204cc95d5a705f28b83dafcee741ff66e
JaocHatter <jarex1012@gmail.com>
2024-08-28 15:04:29 -0500

    Hello, World!

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ app.get("/",(req,res) => {
     res.send("Hello World!");
 });
 
-const port = process.env.port||3000; //Proceso trabajado en el puerto 3000
+const port = process.env.PORT||3000; //Proceso trabajado en el puerto 3000
 
 app.listen(port,()=>{
-    console.log("Server running on port ${port}");
+    console.log("Server running on port %i",port);
 });
 
-module.exports = app;
+module.exports = app;   
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,5 @@
-const { request } = require("express");
-const require = require("supertest");
-const app = express("/home/jared/Desktop/Universidad/6toCiclo/DesarrolloSoftware/Actividad1/devops-practice/src/app.js");
+const request = require("supertest");
+const app = express("../src/app.js");
 
 //describe(...): Es una función proporcionada por Jest que agrupa varias pruebas relacionadas. 
 //En este caso, agrupa las pruebas relacionadas con la ruta GET /
@@ -8,9 +7,10 @@ describe("GET /",() =>{
     //it(...): Es otra función proporcionada por Jest, usada para definir una prueba individual.
     it("Should return Hello World!",async ()=>{
         //nos retorna la respuesta del servidor ante nuestra solicitud!
-        const response = request(app).get("/");
+        const response = await request(app).get("/");
         //Pruebas
         expect(respose.statusCode).toEqual(200); //nos retorne 200 httpStatus
-        expect(response.text).toBe  ("Hello World!") //nuestro Hello World
+        expect(response.text).toBe("Hello World!"); //nuestro Hello World
     });
 });
+
