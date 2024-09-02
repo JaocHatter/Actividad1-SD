bcd007736346c6676d6e1114eea999607280deed
JaocHatter <jarex1012@gmail.com>
2024-09-02 01:00:49 -0500

    fixed

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
+// De esta manera importas la libreria express para hacer aplicaciones web
 const express = require("express");
 //Instanciamos la variables express!
 const app = express();
 
+
+// PRIMER ENDPOINT
 //app.get(enpoint, (REQues,RESponse))
 app.get("/",(req,res) => {
     res.send("Hello World!");
 });
 
-const port = process.env.PORT||3000; //Proceso trabajado en el puerto 3000
-
-app.listen(port,()=>{
-    console.log("Server running on port %i",port);
+//SEGUNDO ENDPOINT
+app.get("/camera_status", (req,res)=>{
+    const camera_status = [
+        {id: 1 , status: "online"},
+        {id: 2, status: "offline"},
+        {id: 3, status: "online"},
+    ];
+    res.json(camera_status)
 });
 
-module.exports = app;   
+module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,9 @@
 const request = require("supertest");
-const app = require("../src/app.js");
+const app = require("../src/app");
 
 //describe(...): Es una función proporcionada por Jest que agrupa varias pruebas relacionadas. 
 //En este caso, agrupa las pruebas relacionadas con la ruta GET /
+
 describe("GET /",() =>{
     //it(...): Es otra función proporcionada por Jest, usada para definir una prueba individual.
     it("Should return Hello World!",async ()=>{
@@ -14,3 +15,10 @@ describe("GET /",() =>{
     });
 });
 
+describe("GET /camera_status",()=>{
+    it("Should return an array with the cameras status", async ()=>{
+        const response = await request(app).get("/camera_status");
+        expect(response.statusCode).toEqual(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+});
